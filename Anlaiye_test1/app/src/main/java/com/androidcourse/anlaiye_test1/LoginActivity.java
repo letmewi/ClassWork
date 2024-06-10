7c6fba904517e7a4193bc9aed1cf6a09a5508bc0
letmewi <2810520180@qq.com>
2024-06-10 13:59:44 +0800

    6.10
    完善了登录界面的错误判断，并增加了记住密码选项，登录注册界面功能初步实现

--- Anlaiye_test1/app/src/main/java/com/androidcourse/anlaiye_test1/LoginActivity.java
+++ Anlaiye_test1/app/src/main/java/com/androidcourse/anlaiye_test1/LoginActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,6 +19,11 @@ import com.androidcourse.anlaiye_test1.entity.UserInfo;
 public class LoginActivity extends AppCompatActivity {
     private EditText et_username;
     private EditText et_password;
+    private CheckBox rm_password;
+    private boolean is_login;
+
+    private SharedPreferences mSharedPreferences;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,6 +34,19 @@ public class LoginActivity extends AppCompatActivity {
         //初始化
         et_username = findViewById(R.id.et_username);
         et_password = findViewById(R.id.et_password);
+        rm_password = findViewById(R.id.remember_password);
+
+        mSharedPreferences = getSharedPreferences("user",MODE_PRIVATE);
+
+        //记住密码状态确认
+        is_login = mSharedPreferences.getBoolean("is_login",false);
+        if (is_login){
+            String username = mSharedPreferences.getString("username",null);
+            String password = mSharedPreferences.getString("password",null);
+            et_username.setText(username);
+            et_password.setText(password);
+            rm_password.setChecked(true);
+        }
 
         //点击注册
         findViewById(R.id.toRegister).setOnClickListener(new View.OnClickListener() {
@@ -47,16 +68,37 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "请输入用户名和密码", Toast.LENGTH_SHORT).show();
                 }else {
                     UserInfo login = UserDbHelper.getInstance(LoginActivity.this).login(username);
-                    if (username.equals(login.getUsername())&& password.equals(login.getPassword())){
-                        //登录成功
-                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                        startActivity(intent);
-                        finish();
+                    if(login!=null){
+                        if (username.equals(login.getUsername())&& password.equals(login.getPassword())){
+
+                            SharedPreferences.Editor editor = mSharedPreferences.edit();
+                            editor.putString("username",username);
+                            editor.putString("password",password);
+                            editor.putBoolean("is_login",is_login);
+                            editor.commit();
+
+                            //登录成功
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+                        }
                     }else {
-                        Toast.makeText(LoginActivity.this, "登录失败", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginActivity.this, "该账号暂未注册", Toast.LENGTH_SHORT).show();
                     }
+
                 }
             }
         });
+
+        //记住密码
+        rm_password.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                is_login = isChecked;
+            }
+        });
+
     }
 }
